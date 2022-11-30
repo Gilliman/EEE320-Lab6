d462834026eb19453123f2e9c3684dd9240a6eef
gilliman <sethgillingham9@gmail.com>
2022-11-29 23:43:15 -0500

    added our own BugKilla file
    not much done in it, just made the file and started to understand it

---
+++ competitors/Brown_Gillingham_BugKilla.py
@@ -0,0 +1,26 @@
+"""
+Our creature that will compete in lab 6
+
+Created by:
+    OCdt Brown, and
+    OCdt Gillingham
+"""
+
+from shared import Creature, Propagator
+
+class BugKilla(Creature):
+
+    __instance_count = 0
+
+    def __init__(self):
+        super().__init__()
+
+
+    def do_turn(self):
+        pass
+
+    def destroyed(self):
+        pass
+
+    def instance_count(self):
+        pass
